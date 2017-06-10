93042c7b959fd4190c8176bb21bea03587cf5f3c
Clement Habinshuti <hello@habbes.xyz>
2017-06-10 22:38:49 +0300

    Add captures to token regexes

--- src/jack_compiler/syntax_analyzer/lexer.clj
+++ src/jack_compiler/syntax_analyzer/lexer.clj
@@ -3,47 +3,49 @@
 
 
 (def token-res
-  {#"class" :keyword
-   #"constructor" :keyword
-   #"function" :keyword
-   #"method" :keyword
-   #"field" :keyword
-   #"static" :keyword
-   #"var" :keyword
-   #"int" :keyword
-   #"char" :keyword
-   #"boolean" :keyword
-   #"void" :keyword
-   #"true" :keyword
-   #"false" :keyword
-   #"null" :keyword
-   #"this" :keyword
-   #"let" :keyword
-   #"do" :keyword
-   #"if" :keyword
-   #"else" :keyword
-   #"while" :keyword
-   #"return" :keyword
+  {#"\s+" :whitespace
 
-   #"\{" :symbol
-   #"\}" :symbol
-   #"\(" :symbol
-   #"\)" :symbol
-   #"\[" :symbol
-   #"\]" :symbol
-   #"\." :symbol
-   #"," :symbol
-   #";" :symbol
-   #"\+" :symbol
-   #"\-" :symbol
-   #"\*" :symbol
-   #"/" :symbol
-   #"&" :symbol
-   #"\|" :symbol
-   #"<" :symbol
-   #">" :symbol
-   #"=" :symbol
-   #"~" :symbol
+   #"(class)" :keyword
+   #"(constructor)" :keyword
+   #"(function)" :keyword
+   #"(method)" :keyword
+   #"(field)" :keyword
+   #"(static)" :keyword
+   #"(var)" :keyword
+   #"(int)" :keyword
+   #"(char)" :keyword
+   #"(boolean)" :keyword
+   #"(void)" :keyword
+   #"(true)" :keyword
+   #"(false)" :keyword
+   #"(null)" :keyword
+   #"(this)" :keyword
+   #"(let)" :keyword
+   #"(do)" :keyword
+   #"(if)" :keyword
+   #"(else)" :keyword
+   #"(while)" :keyword
+   #"(return)" :keyword
+
+   #"(\{)" :symbol
+   #"(\})" :symbol
+   #"(\()" :symbol
+   #"(\))" :symbol
+   #"(\[)" :symbol
+   #"(\])" :symbol
+   #"(\.)" :symbol
+   #"(,)" :symbol
+   #"(;)" :symbol
+   #"(\+)" :symbol
+   #"(\-)" :symbol
+   #"(\*)" :symbol
+   #"(/)" :symbol
+   #"(&)" :symbol
+   #"(\|)" :symbol
+   #"(<)" :symbol
+   #"(>)" :symbol
+   #"(=)" :symbol
+   #"(~)" :symbol
 
    #"(\d+)" :integerConstant
    #"\"((?!\").*)\"" :stringConstant
