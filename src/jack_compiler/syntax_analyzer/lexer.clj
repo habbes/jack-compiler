ffc5878afa845a974faeb0a2a499577829045e45
Clement Habinshuti <hello@habbes.xyz>
2017-06-14 09:20:46 +0300

    Use vector for token-rules to retain order

--- src/jack_compiler/syntax_analyzer/lexer.clj
+++ src/jack_compiler/syntax_analyzer/lexer.clj
@@ -4,56 +4,56 @@
   (:import [jack_compiler.syntax_analyzer.token Token]))
 
 (def token-rules
-  {#"^(\s+)" :whitespace
-   #"^(//\.*)" :whitespace
+  [[#"^(\s+)" :whitespace]
+   [#"^(//\.*)" :whitespace]
    ;TODO: add regex for multiline comment as whitespace
 
-   #"^(class)" :keyword
-   #"^(constructor)" :keyword
-   #"^(function)" :keyword
-   #"^(method)" :keyword
-   #"^(field)" :keyword
-   #"^(static)" :keyword
-   #"^(var)" :keyword
-   #"^(int)" :keyword
-   #"^(char)" :keyword
-   #"^(boolean)" :keyword
-   #"^(void)" :keyword
-   #"^(true)" :keyword
-   #"^(false)" :keyword
-   #"^(null)" :keyword
-   #"^(this)" :keyword
-   #"^(let)" :keyword
-   #"^(do)" :keyword
-   #"^(if)" :keyword
-   #"^(else)" :keyword
-   #"^(while)" :keyword
-   #"^(return)" :keyword
+   [#"^(class)" :keyword]
+   [#"^(constructor)" :keyword]
+   [#"^(function)" :keyword]
+   [#"^(method)" :keyword]
+   [#"^(field)" :keyword]
+   [#"^(static)" :keyword]
+   [#"^(var)" :keyword]
+   [#"^(int)" :keyword]
+   [#"^(char)" :keyword]
+   [#"^(boolean)" :keyword]
+   [#"^(void)" :keyword]
+   [#"^(true)" :keyword]
+   [#"^(false)" :keyword]
+   [#"^(null)" :keyword]
+   [#"^(this)" :keyword]
+   [#"^(let)" :keyword]
+   [#"^(do)" :keyword]
+   [#"^(if)" :keyword]
+   [#"^(else)" :keyword]
+   [#"^(while)" :keyword]
+   [#"^(return)" :keyword]
 
-   #"^(\{)" :symbol
-   #"^(\})" :symbol
-   #"^(\()" :symbol
-   #"^(\))" :symbol
-   #"^(\[)" :symbol
-   #"^(\])" :symbol
-   #"^(\.)" :symbol
-   #"^(,)" :symbol
-   #"^(;)" :symbol
-   #"^(\+)" :symbol
-   #"^(\-)" :symbol
-   #"^(\*)" :symbol
-   #"^(/)" :symbol
-   #"^(&)" :symbol
-   #"^(\|)" :symbol
-   #"^(<)" :symbol
-   #"^(>)" :symbol
-   #"^(=)" :symbol
-   #"^(~)" :symbol
+   [#"^(\{)" :symbol]
+   [#"^(\})" :symbol]
+   [#"^(\()" :symbol]
+   [#"^(\))" :symbol]
+   [#"^(\[)" :symbol]
+   [#"^(\])" :symbol]
+   [#"^(\.)" :symbol]
+   [#"^(,)" :symbol]
+   [#"^(;)" :symbol]
+   [#"^(\+)" :symbol]
+   [#"^(\-)" :symbol]
+   [#"^(\*)" :symbol]
+   [#"^(/)" :symbol]
+   [#"^(&)" :symbol]
+   [#"^(\|)" :symbol]
+   [#"^(<)" :symbol]
+   [#"^(>)" :symbol]
+   [#"^(=)" :symbol]
+   [#"^(~)" :symbol]
 
-   #"^(\d+)" :integerConstant
-   #"^\"((?!\").*)\"" :stringConstant
+   [#"^(\d+)" :integerConstant]
+   [#"^\"((?!\").*)\"" :stringConstant]
 
-   #"^([A-Za-z_][A-Za-z0-9_]*)" :identifier})
+   [#"^([A-Za-z_][A-Za-z0-9_]*)" :identifier]])
 
 (defn- remove-match
   "Removes the match m from the beginning of s"
