9bc36d2dd4145c259a6da26fd81ed5577f0c6bed
Clement Habinshuti <hello@habbes.xyz>
2017-06-14 09:06:56 +0300

    Create xml token writer

---
+++ src/jack_compiler/syntax_analyzer/token-xml-writer.clj
@@ -0,0 +1,44 @@
+(ns jack-compiler.syntax-analyzer.token-xml-writer
+  (:require [jack-compiler.syntax-analyzer.lexer :refer [TokenWriter]]
+            [jack-compiler.syntax-analyzer.token :as tk]))
+
+
+(defn convert-symbol
+  "Converts symbol to corresponding xml entity"
+  [v]
+  (case v
+    "<" "&lt;"
+    ">" "&gt;"
+    "\"" "&quot;"
+    "&" "&amp;"
+    v))
+
+(defn token-to-xml-value
+  "Converts token value to xml-escaped value"
+  [{v :value :as t}]
+  (if (tk/is-type? t :symbol)
+    (convert-symbol v)
+    v))
+
+(defn token-to-xml-element
+  "Returns a string representing the token
+  as an xml element"
+  [{t :type :as token}]
+  (str "<" (name t) ">"
+       (token-to-xml-value token)
+       "</" (name t) ">"))
+
+(deftype XmlWriter []
+  TokenWriter
+  (write-tokens
+    [_ ts w]
+    (.write w "<tokens>")
+    (doseq [t ts]
+      (.write w (token-to-xml-element (t))))
+    (.write w "</tokens>")))
+
+(defn xml-writer
+  "Returns an instance of XmlWriter"
+  (XmlWriter.))
+
+
