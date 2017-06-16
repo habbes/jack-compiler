2590649bd19ff1445e1395eab59c34bb155e1a25
Clement Habinshuti <hello@habbes.xyz>
2017-06-16 09:11:38 +0300

    Add test program for lexer

--- src/jack_compiler/core.clj
+++ src/jack_compiler/core.clj
@@ -1,7 +1,16 @@
 (ns jack-compiler.core
+  (:require [jack-compiler.syntax-analyzer.core :as sa]
+            [jack-compiler.syntax-analyzer.xml-writer :refer [xml-writer]])
   (:gen-class))
+
+(defn test-lexer
+  [dir]
+  (println "Lexing" dir)
+  (let [h (xml-writer)]
+    (sa/lex-and-handle-dir h dir))
+  (println "Done."))
 
 (defn -main
   "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+  [dir]
+  (test-lexer dir))
