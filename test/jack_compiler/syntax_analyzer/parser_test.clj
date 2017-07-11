7b1a4fb02b61c9d8cf702e5e74709b4b9b28acc8
Clement Habinshuti <hello@habbes.xyz>
2017-07-11 09:17:42 +0300

    Add tests for parser/consume-terminal

--- test/jack_compiler/syntax_analyzer/parser_test.clj
+++ test/jack_compiler/syntax_analyzer/parser_test.clj
@@ -1,7 +1,13 @@
 (ns jack-compiler.syntax-analyzer.parser-test
   (:require [clojure.test :refer :all]
             [jack-compiler.syntax-analyzer.parser :refer :all]
-            [jack-compiler.syntax-analyzer.token :as tk]))
+            [jack-compiler.syntax-analyzer.token :as tk]
+            [jack-compiler.syntax-analyzer.parse-tree :as pt]))
+
+; shortcut for token constructor
+(def tkc tk/->Token)
+; shortcut for ParseTree constructor
+(def ptc pt/->ParseTree)
 
 (deftest consume-token-test
   (testing "Returns first token from seq and remaining seq"
@@ -10,3 +16,22 @@
           [t ts-rest] (consume-token ts)]
       (is (= t (tkfn :keyword "int")))
       (is (= ts-rest [(tkfn :identifier "x")])))))
+
+(deftest consume-terminal-test
+  (let [ts [(tkc :keyword "int") (tkc :identifier "x")]]
+    (testing "Returns ParseTree node from matched token and remaining sq"
+      (let [[p ts-rest] (consume-terminal ts :keyword)]
+        (is (= p (ptc :keyword nil "int")))
+        (is (= ts-rest [(tkc :identifier "x")]))))
+    (testing "Throws exception if unexpected type"
+      (is (thrown-with-msg? Exception
+                            #"symbol expected but found keyword int"
+                            (consume-terminal ts :symbol))))
+    (testing "Returns ParseTree node from matched token and value and remaining sq"
+      (let [[p ts-rest] (consume-terminal ts :keyword "int")]
+        (is (= p (ptc :keyword nil "int")))
+        (is (= ts-rest [(tkc :identifier "x")]))))
+    (testing "Throws exception if unexpected value"
+      (is (thrown-with-msg? Exception
+                            #"class expected but found int"
+                            (consume-terminal ts :keyword "class"))))))
