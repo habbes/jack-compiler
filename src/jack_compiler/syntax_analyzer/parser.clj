57a400360a1479af82828f5deb59e45dd3e04626
Clement Habinshuti <hello@habbes.xyz>
2017-07-24 10:18:14 +0300

    Refactor parser code

--- src/jack_compiler/syntax_analyzer/parser.clj
+++ src/jack_compiler/syntax_analyzer/parser.clj
@@ -33,14 +33,34 @@
        (throw-parser-error
          (str  (s/join " or " values) " expected but found " (:value node)))))))
 
+(defn consume-identifier
+  "Consumes next token from ts if it's an identifier."
+  [ts]
+  (consume-terminal ts :identifier))
+
+(defn consume-keyword
+  "Consumes next token from ts if it's a keyword."
+  ([ts]
+   (consume-terminal ts :keyword))
+  ([ts values]
+   (consume-terminal ts :keyword values)))
+
+(defn consume-symbol
+  "Consumes next token from ts if it's a symbol."
+  ([ts]
+   (consume-terminal ts :symbol))
+  ([ts values]
+   (consume-terminal ts :symbol values)))
+
 (defn consume-type
-  "Consumes a type name or class identifier token"
+  "Consumes next token from ts if it's type name keyword
+  or an identifier"
   [[t :as ts]]
   (if (tk/is-type? t :keyword)
-    (consume-terminal :keyword ["int" "char" "boolean"])
-    (consume-terminal :identifier)))
+    (consume-keyword ts ["int" "char" "boolean"])
+    (consume-identifier ts)))
 
-(def consume-comma-var-seq
+(defn consume-comma-var-seq
   "Consume 0 or more pairs of comma followed by a var name.
   Returns a seq of the consumed tokens and seq of remaining
   tokens."
@@ -49,23 +69,23 @@
   ([[t :as ts] vars]
     (if (tk/is-value? t ";")
       [vars ts]
-      (let [[comma ts] (consume-terminal :symbol [","])
-            [iden ts] (consume-terminal :identifier)]
+      (let [[comma ts] (consume-symbol ts [","])
+            [iden ts] (consume-identifier ts)]
         (recur ts (conj vars comma iden))))))
 
 (defn consume-var-seq
   "Consumes a sequence of var names separated by a comma
   and ending in a semicolon"
   [ts]
-  (let [[fst ts] (consume-terminal ts :identifier)
+  (let [[fst ts] (consume-identifier ts)
         [others ts] (consume-comma-var-seq ts)
-        [sc ts] (consume-terminal ts :symbol [";"])]
+        [sc ts] (consume-symbol ts [";"])]
     [(flatten [fst others sc]) ts]))
 
 (defn parse-class-var-dec
   "Parses a classVarDec node"
   [ts]
-  (let [[field ts] (consume-terminal ts :keyword ["field" "static"])
+  (let [[field ts] (consume-keyword ts ["field" "static"])
         [typ ts] (consume-type ts)
         [vars ts] (consume-var-seq ts)]
     (pt/->ParseTree :classVarDec ((comp vec flatten conj)
@@ -82,17 +102,15 @@
   on the specified parser f."
   [ts f])
 
-
-
 (defn parse-class
   "Parses a class node"
   [ts]
-  (let [[cls ts] (consume-terminal ts :keyword ["class"])
-        [id ts] (consume-terminal ts :identitier)
-        [open-br ts] (consume-terminal ts :symbol ["{"])
+  (let [[cls ts] (consume-keyword ts ["class"])
+        [id ts] (consume-identifier ts)
+        [open-br ts] (consume-symbol ts ["{"])
         [vars ts] (parse-* ts parse-class-var-dec)
         [subrs ts] (parse-* ts parse-subroutine-dec)
-        [close-br ts] (consume-terminal ts :symbol ["}"])]
+        [close-br ts] (consume-symbol ts ["}"])]
     (pt/->ParseTree :class ((comp vec flatten conj)
                             cls id open-br vars subrs close-br)
                     nil)))
