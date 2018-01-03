2dce1c3b69ca7b99c551dd7a696b943972df4446
Clément Habinshuti <hello@habbes.xyz>
2018-01-03 20:10:18 +0300

    Add symbol table tests for var-count

--- test/jack_compiler/syntax_analyzer/scoped_symbol_table_test.clj
+++ test/jack_compiler/syntax_analyzer/scoped_symbol_table_test.clj
@@ -24,3 +24,19 @@
              {"y" (symt/->TableEntry "y" "String" :local 0)}))
       (testing "and increments segment counter"
         (is (= (-> tbl .subroutine .counts :local) 1))))))
+
+(deftest var-count-test
+  (testing "Counts segment vars in class scope"
+    (let [tbl (init-table)
+          tbl (add-class-entry tbl {:name "bar" :type "Foo" :kind :field})
+          tbl (add-class-entry tbl {:name "baz" :type "String" :kind :static})
+          tbl (add-class-entry tbl {:name "x" :type "int" :kind :field})]
+      (is (= (class-var-count tbl :field) 2))
+      (is (= (class-var-count tbl :static) 1))))
+  (testing "Counts segment vars in subroutine scope"
+    (let [tbl (init-table)
+          tbl (add-subroutine-entry tbl {:name "size" :type "int" :kind :arg})
+          tbl (add-subroutine-entry tbl {:name "y" :type "String" :kind :local})
+          tbl (add-subroutine-entry tbl {:name "obj" :type "MyClass" :kind :local})]
+      (is (= (sub-var-count tbl :arg) 1))
+      (is (= (sub-var-count tbl :local) 2)))))
