46204ce21da4038ae24f53e5793fff7432e4331b
Clément Habinshuti <hello@habbes.xyz>
2018-01-03 19:08:20 +0300

    Create test file for scoped symbol table

---
+++ test/jack_compiler/syntax_analyzer/scoped_symbol_table_test.clj
@@ -0,0 +1,24 @@
+(ns jack-compiler.syntax-analyzer.scoped-symbol-table-test
+  (:require [clojure.test :refer :all]
+            [jack-compiler.syntax-analyzer.scoped-symbol-table :refer :all]
+            [jack-compiler.syntax-analyzer.symbol-table :as symt]))
+
+(deftest init-table-test
+  (testing "Creates record with class and subroutine scope tables"
+    (let [tbl (init-table)]
+      (is (= (.class tbl) (symt/symbol-table)))
+      (is (= (.subroutine tbl) (symt/symbol-table))))))
+
+(deftest add-entry-test
+  (testing "Adds entry to class scope"
+    (let [tbl (init-table)
+          tbl (add-class-entry tbl {:name "x" :type "int" :kind :field })]
+      (is (= (-> tbl .class .entries)
+             {"x" (symt/->TableEntry "x" "int" :field 0)}))
+      (is (= (-> tbl .class .counts :field) 1))))
+  (testing "Adds entry to subroutine scope"
+    (let [tbl (init-table)
+          tbl (add-subroutine-entry tbl {:name "y" :type "String" :kind :local})]
+      (is (= (-> tbl .subroutine .entries)
+             {"y" (symt/->TableEntry "y" "String" :local 0)}))
+      (is (= (-> tbl .subroutine .counts :local) 1)))))
